19c60f4b630ce2f7a334dd961c99e32f82f0ff20
Flandre3569 <1332726596@qq.com>
2022-03-28 23:01:53 +0800

    [update] module - profile data

--- src/views/main/ProfilePage.vue
+++ src/views/main/ProfilePage.vue
@@ -3,6 +3,15 @@ import { ref } from "vue";
 const imgSrc = ref(
   "http://mingxuan3569.oss-cn-beijing.aliyuncs.com/image/38O1Z%5DE5G4%7D_%60EO9KPK8%5B7T.bmp"
 );
+
+const data = [
+  "Racing car sprays burning fuel into crowd.",
+  "Japanese princess to wed commoner.",
+  "Australian walks 100km after outback crash.",
+  "Man charged over missing wedding girl.",
+  "Los Angeles battles huge wildfires.",
+];
+const name = ref("Gala");
 </script>
 
 <template>
@@ -11,7 +20,18 @@ const imgSrc = ref(
       <div class="avatar w-2/5 px-10 pt-10">
         <img :src="imgSrc" alt="" class="avatar-img" />
       </div>
-      <div></div>
+      <div>
+        <h1 class="mt-10">hello</h1>
+        <span>i am {{ name }}</span>
+        <div class="mt-10">
+          <ul v-for="item in data" :key="item">
+            <li class="flex justify-start space-x-5">
+              <h2>{{ item }}</h2>
+              <span>{{ item }}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
     <div class="bg-top h-1/4"></div>
 
